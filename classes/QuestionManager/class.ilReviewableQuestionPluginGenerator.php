bf57e6689c40f4c708a2111d8347a8cd51979837
minacode <minamoto9@web.de>
2015-03-07 17:45:31 +0100

    sry, ich versuch das so schnell wie möglich hinzubekommen

--- classes/QuestionManager/class.ilReviewableQuestionPluginGenerator.php
+++ classes/QuestionManager/class.ilReviewableQuestionPluginGenerator.php
@@ -1,32 +1,33 @@
 <?php
-class ilReviewableQuestionPluginGenerator() {
+class ilReviewableQuestionPluginGenerator {
     
-    private $instance = null;
+    private static $instance = null;
     
     private function _construct() {}
+    private function _clone() {}
     
     public static function get() {
-        if ($this->instance == null) {
-            $this->instance = new ilReviewableQuestionPluginGenerator();
+        if (self::$instance == null) {
+            self::$instance = new self;
         }
-        return $this->instance;
+        return self::$instance;
     }
     
-    private function get_question_type_id( $question_type ) {
-        global ilDB;
+    private function getQuestionTypeId( $question_type ) {
+        global $ilDB;
         
-        $result = ilDB->query('SELECT question_type_id FROM qpl_qst_type WHERE type_tag == "ass'. $question_type .'"');
-        if ( $result = ilDB->fetchAssoc( $result ) ) {
+        $result = $ilDB->query('SELECT question_type_id FROM qpl_qst_type WHERE type_tag LIKE "ass'. $question_type .'"');
+        if ( $result = $ilDB->fetchAssoc( $result ) ) {
             return $result['question_type'];
         } 
         return null;
     }
     
-    private function get_question_type_path( $question_type ) {
-        global ilDB;
+    private function getQuestionTypePath( $question_type ) {
+        global $ilDB;
         
-        $result = ilDB->query('SELECT plugin FROM qpl_qst_type WHERE type_tag == "ass'. $question_type .'"');
-        if ( $result = ilDB->fetchAssoc( $result ) ) {
+        $result = $ilDB->query('SELECT plugin FROM qpl_qst_type WHERE type_tag LIKE "ass'. $question_type .'"');
+        if ( $result = $ilDB->fetchAssoc( $result ) ) {
             if ($result['plugin']) {
                 return '.Modules/TestQuestionPool/Questions/ass'. $question_type . '/classes/';
             } else {
@@ -37,9 +38,9 @@ class ilReviewableQuestionPluginGenerator() {
     }
     
     
-    private function calculate_placeholder_values( $question_type ) {
-        $id = this->get_question_type_id( $question_type_id );
-        $path = this->get_question_type_path( $question_type );
+    private function calculatePlaceholderValues( $question_type ) {
+        $id = $this->getQuestionTypeId( $question_type_id );
+        $path = $this->getQuestionTypePath( $question_type );
         if ( $id && $path ) {
             return array(
                 '<id>'      => 'rev' . $id,
@@ -48,32 +49,32 @@ class ilReviewableQuestionPluginGenerator() {
                 '<resp>'    => 'Max Friedrich, Richard Mörbitz',
                 '<respm>'   => 'max.friedrich@tu-dresden.de, richard.moerbitz@tu-dresden.de',
                 '<qtype>'   => $question_type,
-                '<qpath>'   => $path;
+                '<qpath>'   => $path
             );
         }
         return array();    
     }
     
-    private function replace_template_placeholders( $template, $placeholder_values ) {
+    private function replaceTemplatePlaceholders( $template, $placeholder_values ) {
         foreach ($placeholder_values as $placeholder => $value) {
             $template = str_replace( $placeholder, $value, $template);
         }
         return $template;
     }
     
-    private function create_file_from_template( $question_type, $template_path, $file_path ) {
+    private function createFileFromTemplate( $question_type, $template_path, $file_path ) {
         $template = file_get_contents( $template_path, "r");
-        $placeholder_values = this->calculate_placeholder_values( $question_type );
-        $template = this->replace_template_placeholders( $template, $placeholder_values );
+        $placeholder_values = $this->calculatePlaceholderValues( $question_type );
+        $template = $this->replaceTemplatePlaceholders( $template, $placeholder_values );
         file_put_contents( $file_path . $file_name, $template );
     }
     
-    public function create_plugin( $question_type, $original_path ) {
-        $question_type = substr( $question_type, 2);
-        $base_path = './Modules/TestquestionPool/Questions/assReviewable'. $question_type .'/';
-        mkdir( $base_path, recursive = true );
+    public function createPlugin( $question_type ) {
+        $question_type = substr( $question_type, 3);
+        $base_path = './Modules/TestQuestionPool/Questions/assReviewable'. $question_type .'/';
+        mkdir( $base_path, true );
         // template_name, file_name, path
-        files = array(
+        $files = array(
             array( 'plugin-php',                            'plugin.php',                           '' ),
             array( 'class.assQuestionType.php',             'class.assReviewable'. $question_type .'php',               'classes/'              ),
             array( 'class.assQuestionTypeGUI.php',          'class.assReviewable'. $question_type .'GUI.php',           'classes/'              ),
@@ -86,10 +87,10 @@ class ilReviewableQuestionPluginGenerator() {
             array( 'class.assQuestionTypeImport.php',       'class.assReviewable'. $question_type .'Import.php',        'classes/import/qti12/' )
         ); 
         foreach( $files as $file ) {
-            mkdir( $base_path . file[2], recursive = true );
+            mkdir( $base_path . file[2], true );
             $template = 'templates/question_files/' . file[0];
             $file = $base_path . file[2] . file[1];
-            this->create_file_from_template( $question_type, $template, $file );
+            $this->createFileFromTemplate( $question_type, $template, $file );
         }
     }
     
